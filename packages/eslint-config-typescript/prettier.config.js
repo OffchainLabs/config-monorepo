0a309e746214bb00543a9c69b48233e3525eee98
Christophe <contact@christophedeveaux.com>
2023-01-26 14:35:25 +0100

    chore: Add @offchainlabs/prettier-config to eslint package

--- packages/eslint-config-typescript/prettier.config.js
+++ packages/eslint-config-typescript/prettier.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  tabWidth: 2,
-  useTabs: false,
-  semi: true,
-  singleQuote: true,
-  bracketSpacing: true,
-  arrowParens: 'always',
-  trailingComma: 'all',
+  ...require('@offchainlabs/prettier-config'),
 };
